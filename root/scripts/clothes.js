b5d296d34e23cb3d266d2f7df32cfc53dccea520
Andi <andi.santos-oliveira@cpnv.ch>
2021-01-11 15:33:12 +0100

    Start project GUI2

---
+++ root/scripts/clothes.js
@@ -0,0 +1,99 @@
+const articles = [
+	{   
+		id: 0,
+		titre: "T-Shirt nike",
+		likes: 10,
+		colors: ["E1C21C", "E11C1C", "698E35"],
+		prom: 50,
+		price: 100.10,
+		sizes: ["XS", "S", "L"],
+		stock: 10,
+		img: ""
+    },
+    {   
+		id: 1,
+		titre: "T-Shirt nike",
+		likes: 10,
+		colors: ["E1C21C", "E11C1C", "698E35"],
+		prom: 50,
+		price: 100.10,
+		sizes: ["XS", "S", "L"],
+		stock: 10,
+		img: ""
+    },
+	{   
+		id: 2,
+		titre: "T-Shirt nike",
+		likes: 10,
+		colors: ["E1C21C", "E11C1C", "698E35"],
+		prom: 50,
+		price: 100.10,
+		sizes: ["XS", "S", "L"],
+		stock: 10,
+		img: ""
+    },
+	{   
+		id: 3,
+		titre: "T-Shirt nike",
+		likes: 10,
+		colors: ["E1C21C", "E11C1C", "698E35"],
+		prom: 50,
+		price: 100.10,
+		sizes: ["XS", "S", "L"],
+		stock: 10,
+		img: ""
+    },
+	{   
+		id: 4,
+		titre: "T-Shirt nike",
+		likes: 10,
+		colors: ["E1C21C", "E11C1C", "698E35"],
+		prom: 50,
+		price: 100.10,
+		sizes: ["XS", "S", "L"],
+		stock: 10,
+		img: ""
+    },
+	{   
+		id: 5,
+		titre: "T-Shirt nike",
+		likes: 10,
+		colors: ["E1C21C", "E11C1C", "698E35"],
+		prom: 50,
+		price: 100.10,
+		sizes: ["XS", "S", "L"],
+		stock: 10,
+		img: ""
+    },
+	{   
+		id: 6,
+		titre: "T-Shirt nike",
+		likes: 10,
+		colors: ["E1C21C", "E11C1C", "698E35"],
+		prom: 50,
+		price: 100.10,
+		sizes: ["XS", "S", "L"],
+		stock: 10,
+		img: ""
+    }
+];
+
+const users = [
+	{   
+		id: 0,
+		username: "Jean BAPT",
+		login: "admin",
+		password: "admin",
+		history: [
+			{
+				date: 10-10-2019,
+				articles: [0, 1, 4]
+			},
+			{
+				date: 10-12-2019,
+				articles: [0, 3, 6]
+			}
+		],
+		cart: [0, 3, 6]
+    }
+];
