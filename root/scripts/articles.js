a13fa773729a73ab34d9b8e5af3c25a8eb7a0780
Andi <andi.santos-oliveira@cpnv.ch>
2021-01-18 15:01:16 +0100

    Update display articles

--- root/scripts/articles.js
+++ root/scripts/articles.js
@@ -1,17 +1,17 @@
 Vue.component('display-item', {
     props: ['item'],
     template: `
-    <div class="col-4">
-        <div class="p-2 card text-white">
-            <img v-bind:src="item.img" class="card-img" alt="">
-            <div class="card-img-overlay">
-                <span class="card-text bg-dark">{{ item.likes }} <i class="fas fa-heart"></i></span>
-            </div>
+        <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+            <a class="card p-2" v-bind:href="'article.html?article=' + item.id">
+                <img v-bind:src="item.img" class="card-img" alt="">
+                <div class="card-img-overlay d-flex">
+                    <span class="card-text bg-light text-dark border border-dark shadow mt-auto ms-auto px-2 py-1">
+                        {{ item.likes }} <i class="fas fa-heart text-danger"></i>
+                    </span>
+                </div>
+            </a>
         </div>
-    </div>
-    `
-    // template: '<div class="men-itm" v-on:click="displayVehicule()">{{ itm.titre }}<i :class="[displayIcon(itm.cat)]" aria-hidden="true"></i></div>'
-})
+    `})
 
 var app = new Vue({
     el: '.container',
